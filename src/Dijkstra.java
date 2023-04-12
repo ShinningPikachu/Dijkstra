aef27ff14e9d96f4d2e61d375a29622b2d04fd4a
Mona Lisa <jinhengl2001@gmail.com>
2023-04-12 15:35:19 +0200

    Important comments

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -1,36 +1,41 @@
 import java.util.*;
 
 public class Dijkstra {
-    private int dist[];
-    private int parent[];
-    private Set<Integer> visited;
-    private PriorityQueue<Node> pq;
-    private int V; // number of vertices
-    List<List<Node>> adj;
+    // instance variables
+    private int dist[]; // an array to hold the shortest distance to each vertex from the source vertex
+    private int parent[]; // an array to hold the parent vertex of each vertex on the shortest path
+    private Set<Integer> visited; // a set to keep track of visited vertices
+    private PriorityQueue<Node> pq; // a priority queue to hold vertices based on their distance from the source vertex
+    private int V; // the number of vertices in the graph
+    List<List<Node>> adj; // the adjacency list of the graph
 
+    // constructor
     public Dijkstra(int V) {
         this.V = V;
         dist = new int[V];
         parent = new int[V];
         visited = new HashSet<Integer>();
-        pq = new PriorityQueue<Node>(V, new Node());
+        pq = new PriorityQueue<Node>(V, new Node()); // initialize the priority queue with capacity V and a new Node comparator
     }
 
+    // Dijkstra's shortest path algorithm
     public void dijkstra(List<List<Node>> adj, int src, int dest) {
         this.adj = adj;
 
+        // initialize dist and parent arrays for all vertices
         for (int i = 0; i < V; i++) {
             dist[i] = Integer.MAX_VALUE;
             parent[i] = -1;
         }
 
-        pq.add(new Node(src, 0));
-        dist[src] = 0;
+        pq.add(new Node(src, 0)); // add the source vertex to the priority queue with distance 0
+        dist[src] = 0; // set the distance of the source vertex to 0
 
+        // main loop of the algorithm
         while (!pq.isEmpty()) {
-            int u = pq.poll().node;
+            int u = pq.poll().node; // get the vertex with the minimum distance from the priority queue
 
-            visited.add(u);
+            visited.add(u); // mark the vertex as visited
 
             if (u == dest) {
                 // backtrack to construct the shortest path
@@ -40,32 +45,36 @@ public class Dijkstra {
                     u = parent[u];
                 }
                 path.add(u);
-                Collections.reverse(path);
+                Collections.reverse(path); // reverse the list to get the correct order
                 System.out.println("Shortest path: " + path);
-                return;
+                return; // we have found the shortest path, so we can return
             }
 
-            e_Neighbours(u);
+            e_Neighbours(u); // check the neighbours of the current vertex
         }
     }
 
+    // helper method to check the neighbours of a vertex
     private void e_Neighbours(int u) {
         int edgeDistance = -1;
         int newDistance = -1;
 
+        // loop through the neighbours of the current vertex
         for (int i = 0; i < adj.get(u).size(); i++) {
             Node v = adj.get(u).get(i);
 
+            // check if the neighbour has not been visited yet
             if (!visited.contains(v.node)) {
                 edgeDistance = v.cost;
                 newDistance = dist[u] + edgeDistance;
 
+                // check if the new distance to the neighbour is less than its current distance
                 if (newDistance < dist[v.node]) {
                     dist[v.node] = newDistance;
-                    parent[v.node] = u;
+                    parent[v.node] = u; // set the parent of the neighbour to the current vertex
                 }
 
-                pq.add(new Node(v.node, dist[v.node]));
+                pq.add(new Node(v.node, dist[v.node])); // add the neighbour to the priority queue with its updated distance
             }
         }
     }
--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,51 @@
+// Importing required packages
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-        public static void main(String[] args) {
-            int V = 6;
-            int source = 0;
-            int dest = 5;
+    public static void main(String[] args) {
+// Setting up the input graph
+        int V = 6; // number of vertices
+        int source = 0; // source node
+        int dest = 5; // destination node
 
-            List<List<Node>> adj = new ArrayList<List<Node>>();
-            for (int i = 0; i < V; i++) {
-                List<Node> item = new ArrayList<Node>();
-                adj.add(item);
-            }
-
-            adj.get(0).add(new Node(1, 1));
-            adj.get(0).add(new Node(2, 1));
-            adj.get(1).add(new Node(3, 1));
-            adj.get(1).add(new Node(2, 1));
-            adj.get(2).add(new Node(4, 1));
-            adj.get(2).add(new Node(5, 1));
-            adj.get(2).add(new Node(3, 1));
-            adj.get(3).add(new Node(4, 1));
-            adj.get(4).add(new Node(5, 1));
-            for(List<Node> i : adj){
-                System.out.println(i);
-            }
-
-            Dijkstra dpq = new Dijkstra(V);
-            dpq.dijkstra(adj, source, dest);
+        // Creating an adjacency list to represent the graph
+        List<List<Node>> adj = new ArrayList<List<Node>>();
+        for (int i = 0; i < V; i++) {
+            List<Node> item = new ArrayList<Node>();
+            adj.add(item);
         }
+
+        // Adding edges to the graph
+        adj.get(0).add(new Node(1, 1));
+        adj.get(1).add(new Node(0, 1));
+        adj.get(0).add(new Node(2, 1));
+        adj.get(2).add(new Node(0, 1));
+        adj.get(1).add(new Node(3, 1));
+        adj.get(3).add(new Node(1, 1));
+        adj.get(1).add(new Node(2, 1));
+        adj.get(2).add(new Node(1, 1));
+        adj.get(2).add(new Node(4, 1));
+        adj.get(4).add(new Node(2, 1));
+        adj.get(2).add(new Node(5, 1));
+        adj.get(5).add(new Node(2, 1));
+        adj.get(2).add(new Node(3, 1));
+        adj.get(3).add(new Node(2, 1));
+        adj.get(3).add(new Node(4, 1));
+        adj.get(4).add(new Node(3, 1));
+        adj.get(4).add(new Node(5, 1));
+        adj.get(5).add(new Node(4, 1));
+
+        // Printing the adjacency list
+        for(List<Node> i : adj){
+            for(Node j: i){
+                System.out.print(j.node + " ");
+            }
+            System.out.println("");
+        }
+
+        // Running Dijkstra's algorithm on the input graph
+        Dijkstra dpq = new Dijkstra(V);
+        dpq.dijkstra(adj, source, dest);
     }
+}
